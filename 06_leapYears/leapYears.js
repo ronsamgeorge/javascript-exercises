ec18ca9e48746d84a72c45372ac85fdfe0aec5bf
Ronald George <ronaldsamgeorge@gmail.com>
2022-04-25 00:57:09 +0530

    Solve Exercise #7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,6 @@
 const leapYears = function(year) {
 
-    if (year % 4 == 0) 
+    /*if (year % 4 == 0) 
     {
         if (year % 400 == 0){
             return true;
@@ -12,7 +12,9 @@ const leapYears = function(year) {
 
     }
 
-    return false;
+    return false; */
+
+    return year % 4 === 0 && ( year%100 !== 0 || year%400 === 0);
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ctof = function(inCelsius) {
+  return Math.round(((inCelsius * (9/5)) + 32) * 10) /10;
 };
 
-const ctof = function() {
-
+const ftoc = function(infahrenheit) {
+    return Math.round(((infahrenheit -32) * (5/9)) *10)/10;
 };
 
 // Do not edit below this line
