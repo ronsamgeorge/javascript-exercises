bc6fe68a52e57e8d98276fb296e93621b233088f
Ronald George <ronaldsamgeorge@gmail.com>
2022-04-24 22:35:43 +0530

    Complete solutions till #4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let result = "";
+
+    for (let i = string.length-1; i >= 0; i--){
+        result += string.charAt(i);
+    }
+
+    return result;
+    
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,27 @@
 const removeFromArray = function() {
 
+    let itemsToSearch = (Array.from(arguments)).slice(1);      //to create an array of the list of arguments except from the Search array at arguments[0]
+    let inputArray = arguments[0];
+
+
+   for(const item of itemsToSearch){
+        let isPresent = inputArray.indexOf(item)
+        console.log(item);
+
+        if(isPresent == -1)
+        {
+            console.log(`Item ${item} is not present`);
+            continue;
+        } else {
+            console.log(`Item : ${item} is present at `);
+            inputArray.splice(isPresent,1);
+        }
+   }
+
+   return inputArray;
 };
 
+
+removeFromArray([10,200,30],10,30);
 // Do not edit below this line
 module.exports = removeFromArray;
